5a2f77abdd0ee7d68b34bc6aea96ef59f02cfa6b
Riad Oustani <riadoustani07@gmail.com>
2026-02-13 16:39:46 +0100

    Refactor booking routes to use ES6 imports

--- car-rental-api/Routes/bookingRoutes.js
+++ car-rental-api/Routes/bookingRoutes.js
@@ -1,11 +1,33 @@
-const express = require('express');
+import express from 'express';
+import {
+  createBooking,
+  getMyBookings,
+  getAllBookings,
+  getBookingById,
+  cancelBooking
+} from '../controllers/bookingController.js';
+import { protect, role } from '../middlewares/authMiddleware.js';
+import { validate } from '../middlewares/validateMiddleware.js';
+import { createBookingValidation } from '../validators/bookingValidator.js';
+import { mongoIdParam } from '../validators/commonValidator.js';
+
 const router = express.Router();
-const {
-    createBooking,
-    getBookings
-} = require('../controllers/bookingController');
 
-router.post('/', createBooking);
-router.get('/', getBookings);
+router.use(protect);
 
-module.exports = router;
+router.post(
+  '/',
+  createBookingValidation,
+  validate,
+  createBooking
+);
+
+router.get('/my', getMyBookings);
+
+router.get('/admin/all', role('admin'), getAllBookings);
+
+router.get('/:id', mongoIdParam, validate, getBookingById);
+
+router.put('/:id/cancel', mongoIdParam, validate, cancelBooking);
+
+export default router;
