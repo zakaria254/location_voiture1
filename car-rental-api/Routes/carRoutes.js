6b588492b4558ffde84778a9cefa8d822006187e
Riad Oustani <riadoustani07@gmail.com>
2026-02-13 16:39:31 +0100

    Refactor carRoutes to use ES6 imports and add validations

--- car-rental-api/Routes/carRoutes.js
+++ car-rental-api/Routes/carRoutes.js
@@ -1,13 +1,49 @@
-const express = require('express');
+import express from 'express';
+import {
+  createCar,
+  getAllCars,
+  getCarById,
+  updateCar,
+  deleteCar
+} from '../controllers/carController.js';
+import { protect, role } from '../middlewares/authMiddleware.js';
+import { validate } from '../middlewares/validateMiddleware.js';
+import {
+  createCarValidation,
+  updateCarValidation
+} from '../validators/carValidator.js';
+import { mongoIdParam } from '../validators/commonValidator.js';
+
 const router = express.Router();
-const {
-    getCars,
-    getCarById,
-    createCar
-} = require('../controllers/carController');
 
-router.get('/', getCars);
-router.get('/:id', getCarById);
-router.post('/', createCar);
+router.get('/', getAllCars);
+router.get('/:id', mongoIdParam, validate, getCarById);
 
-module.exports = router;
+router.use(protect);
+
+router.post(
+  '/',
+  role('admin'),
+  createCarValidation,
+  validate,
+  createCar
+);
+
+router.put(
+  '/:id',
+  role('admin'),
+  mongoIdParam,
+  updateCarValidation,
+  validate,
+  updateCar
+);
+
+router.delete(
+  '/:id',
+  role('admin'),
+  mongoIdParam,
+  validate,
+  deleteCar
+);
+
+export default router;
